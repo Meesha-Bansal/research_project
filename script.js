1e0b962d0aee685dd4a17c84d947343bdcbd5bf4
Meesha-Bansal <meeshabansal54@gmail.com>
2026-03-11 10:58:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 // GLB path (served from Vite/public at site root)
 const MODEL_URL = 'bloodflow6.glb';
 
@@ -272,4 +274,5 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
+
 
